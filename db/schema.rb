c0c70205c6fecdaca95fabd98d02207578f3fc42
burnsbeaver <burnsbeaver@gmail.com>
2017-08-30 12:18:32 -0400

    restructured the schema THROUGH migrations. Made the post schema reference users and cities via foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830151740) do
+ActiveRecord::Schema.define(version: 20170830161638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,11 +23,14 @@ ActiveRecord::Schema.define(version: 20170830151740) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "user_id"
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "city_id"
+    t.bigint "user_id"
+    t.index ["city_id"], name: "index_posts_on_city_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +50,6 @@ ActiveRecord::Schema.define(version: 20170830151740) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "cities"
+  add_foreign_key "posts", "users"
 end
