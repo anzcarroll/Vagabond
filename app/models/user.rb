dc4a47372c89428e0047ed9b0e01f2292aee1126
jsmatty <jscot.matty@gmail.com>
2017-09-01 10:45:42 -0400

    testing gem paperclip, added avatar to devise application controller. *issue: image file name will display, image will not.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_many :cities, through: :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/default_image.png"
+   has_attached_file :avatar, :styles => { :medium => "300x300>" }, :default_url => "/images/:style/default_image.png"
         validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
