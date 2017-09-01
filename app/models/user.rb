9898e475df3ad7684cf1c24c7b347ffeafff9b8d
jsmatty <jscot.matty@gmail.com>
2017-09-01 02:33:31 -0400

    added button to allow users to upload image, *issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/default_image.png"
-        validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+        validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
